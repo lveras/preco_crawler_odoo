9d2e27a11872b9b19ecd1a45cff0fb79c3855fb3
Luciano <luciano.veras@abgf.gov.br>
2019-06-06 18:53:46 -0300

    [FIX] CPF no usuário pelo controller

--- specific-parts/specific-addons/sce/models/res_partner.py
+++ specific-parts/specific-addons/sce/models/res_partner.py
@@ -4,13 +4,14 @@ from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
-
     _inherit = "res.partner"
+    _sql_constraints = [
+        ('cpf_unique', 'unique(cpf)', 'CPF já cadastrado!')]
 
     cpf = fields.Char(
-        string='CPF'
+        string='CPF',
+        requered=True,
     )
-
 
     @api.model
     def create(self, vals):
--- specific-parts/specific-addons/sce/models/res_users.py
+++ specific-parts/specific-addons/sce/models/res_users.py
@@ -4,15 +4,11 @@ from odoo import models, fields, api
 
 
 class ResUsers(models.Model):
-
     _inherit = "res.users"
-
-    cpf = fields.Char(
-        string='CPF',
-        related="partner_id.cpf",
-    )
 
     @api.model
     def create(self, vals):
-        print('oba')
-        return super(ResUsers, self).create(vals)
+        res = super(ResUsers, self).create(vals)
+        res.partner_id.cpf = vals['cpf']
+
+        return res
