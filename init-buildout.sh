5a82964b8b284c69c80290ea03993289dadf7721
Luciano <luciano.veras@abgf.gov.br>
2019-06-06 12:33:11 -0300

    [FIX] Buildout limpo

--- init-buildout.sh
+++ init-buildout.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e -x
 sudo apt-get install python3 python-dev python3-dev
+git clone git@gitlab.abgf.gov.br:e-sce/odoo12.git .
 virtualenv -p python3 .
 bin/pip install -U zc.buildout pip
 bin/buildout
